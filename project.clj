b2c15e8a6f8d15283b1af912423114a785e82546
Carmen La <carmen.wla@gmail.com>
2016-05-26 16:04:37 -0400

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clipchat "1.0.0-SNAPSHOT"
   :description "Clojure wrapper for HipChat API"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.0"]
-                 [clj-http "0.5.8"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "1.1.2"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
